0dc07afb58b28532782d61da3c75ba8cb9e53cf8
RedSun-ops <55594856+RedSun-ops@users.noreply.github.com>
2019-11-28 21:19:03 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  echo date('d.m.y');
+  echo ' <br/>';
+  $a = array(
+          "dev1" => 'Supergoldieblox17');
+  $b = "Coming Soon...";
+  echo $a["dev1"];
+  echo '<hr>';
+  echo 'version 0.0.0.1 <br/>';
+  echo ' <br/>';
+  echo $b;
+  echo '<a href="http://google.com">source code</a>'
+?>
